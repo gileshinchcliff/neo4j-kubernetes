cdc35abe39a8f5a0fd8278108f96f47a28ce5e62
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-06-16 15:46:09 +0100

    added healthcheck to container

---
+++ healthcheck.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+httptest=$(curl -w %{http_code} -s --output /dev/null localhost:7474)
+if [ "netstat -plunt | grep 5001" ]; then
+  echo "successfully connected to port 5001"
+  if [ "netstat -plunt | grep 6001" ]; then  
+    echo "successfully connected to port 6001"
+    if [[ $httptest -ne 200 ]]; then
+     if [ -f /first_run ]; then 
+       echo "lost connection to port 7474"
+       exit 1 
+     else
+       echo "still creating a cluster exiting successfully"
+       exit 0
+     fi
+   else
+     echo "connected to port 7474 successfully"
+     exit 0 
+   fi
+  else 
+    echo "port 6001 not available exiting"   
+    exit 1
+  fi
+else 
+  echo "port 5001 not available exiting"
+  exit 1
+fi
