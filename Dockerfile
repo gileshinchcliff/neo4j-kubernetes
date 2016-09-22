44bf9bcc7317a8e3687f3a630d8ce4b39717d965
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-09-22 11:48:58 +0100

    updated with new hosts

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
-FROM neo4j:3.0.4-enterprise
+FROM neo4j:3.0.6-enterprise
 
 RUN apt-get update && apt-get -y install net-tools awscli
 ADD healthcheck.sh /healthcheck.sh
 ADD docker-entrypoint.sh /docker-entrypoint.sh
 ADD neo4j.conf /var/lib/neo4j/conf/neo4j.conf
-ADD migration.sh /migration.sh
--- healthcheck.sh
+++ healthcheck.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
-httptest=$(curl -w %{http_code} -s --output /dev/null localhost:7474)
-if [ "netstat -plunt | grep 5001" ]; then
-  echo "successfully connected to port 5001"
-  if [ "netstat -plunt | grep 6001" ]; then  
-    echo "successfully connected to port 6001"
-    if [[ $httptest -ne 200 ]]; then
-     if [ -f /first_run ]; then 
-       echo "lost connection to port 7474"
-       exit 1 
-     else
-       echo "still creating a cluster exiting successfully"
-       
-       exit 0
-     fi
-    else
-     if [ -f /first_run ]; then
-      echo "connected to port 7474 successfully"
-      exit 0 
-     else 
-      touch /first_run
-      echo "connected to port 7474 for the first time"
-      exit 0
-     fi
-    fi
-  else 
-    echo "port 6001 not available exiting"   
+#!/bin/bash
+
+STATUSCODE=$(curl -k -L --silent --output /dev/null --write-out "%{http_code}" https://127.0.0.1:7473/)
+
+if test $STATUSCODE -ne 200; then
+    echo "FAILURE"
     exit 1
-  fi
-else 
-  echo "port 5001 not available exiting"
-  exit 1
+else
+    echo "SUCCESS"
+    exit 0
 fi
